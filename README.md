a8e36df925bfff485340504bb012c7e4d9895044
Samuel Svensson <samme.s90@gmail.com>
2024-05-06 17:33:27 +0200

    Add functionality to send data

---
+++ README.md
@@ -0,0 +1,28 @@
+# Pico
+
+## Setup
+
+All commands and paths are relative to the project root.
+
+### Environment
+
+```py
+# config.py
+#
+# ******************** IMPORTANT! ********************
+# *** This file should never be version controlled ***
+# ****************************************************
+
+# WiFi
+SSID        = ""
+SSID_SECRET = ""
+
+# Adafruit IO
+ADA_USER    = ""
+ADA_SECRET  = ""
+ADA_TOPIC   = f"{ADA_USER}/feeds/topic"
+
+# MQTT
+HOST = "io.adafruit.com"
+PORT = 1883
+```
--- main.py
+++ main.py
@@ -1,17 +1,123 @@
-import dht
+import machine
 import utime
-from machine import Pin
+from dht import DHT11
+from network import STA_IF, WLAN
+from ubinascii import hexlify
 
-run = True
-led = Pin("WL_GPIO0", Pin.OUT)
-sensor = dht.DHT11(Pin(28, Pin.OUT))
+from config import (ADA_SECRET, ADA_TOPIC, ADA_USER, HOST, PORT, SSID,
+                    SSID_SECRET)
+from extensions.umqttsimple import MQTTClient, MQTTException
 
-while run:
-    sensor.measure()
 
-    utime.sleep(1)
+class WLANController:
+    def __init__(self, ssid: str, ssid_secret: str, name="WLAN"):
+        if ssid is None or ssid_secret is None:
+            raise ValueError("Service set -identifier (network name) or -secret (password) is incorrect")
 
-    temp = sensor.temperature()
-    humidity = sensor.humidity()
-    print(f"Temperature: {temp}C")
-    print(f"Humidity: {humidity}%")
+        # In wireless networking (specifically the IEEE 802.11 standards that
+        # define Wi-Fi), a station (STA) interface (IF) refers to any device
+        # that can connect to a wireless network.
+        self.sta_if = WLAN(STA_IF)
+        self.ssid = ssid
+        self.ssid_secret = ssid_secret
+        self.name = name
+        self.__connect()
+
+    def __connect(self):
+        if not self.sta_if.isconnected():
+            print(f"{self.name} :: Connecting to router...")
+            self.sta_if.active(True)
+            self.sta_if.connect(self.ssid, self.ssid_secret)
+
+            while not self.sta_if.isconnected():
+                print(f"{self.name} :: Waiting for connection...")
+                utime.sleep(5)
+
+        print(f"{self.name} :: Connected :: Config<{self.sta_if.ifconfig()}>")
+
+    def disconnect(self):
+        if self.sta_if.isconnected():
+            print(f"{self.name} :: Disconnecting from router...")
+            self.sta_if.disconnect()
+            print(f"{self.name} :: Disconnected")
+        else:
+            print(f"{self.name} :: No active connection to disconnect")
+
+
+class MQTTController:
+    def __init__(self, client_id: bytes, host: str, port: int, user: str, secret: str, name="MQTT"):
+        self.client = MQTTClient(
+            client_id,
+            host,
+            port,
+            user,
+            secret,
+            keepalive=60)
+        self.info = f"{client_id.decode()}@{host}:{port}"
+        self.name = name
+        self.__connect()
+
+    def __connect(self):
+        try:
+            print(f"{self.name} :: Connecting :: {self.info}")
+            self.client.connect()
+            print(f"{self.name} :: Connection established")
+        except Exception as e:
+            if isinstance(e, MQTTException):
+                print(f"{self.info} :: MQTTException :: {str(e)}")
+            elif isinstance(e, OSError):
+                print(f"{self.info} :: OSError :: {str(e)}")
+            else:
+                print(f"{self.info} :: Exception :: {str(e)}")
+            self.__reset()
+
+    def __reset(self, seconds=5):
+        print(f"Resetting in: {seconds}s")
+        utime.sleep(seconds)
+        machine.reset()
+
+    def publish(self, topic: bytes, msg: bytes):
+        self.client.publish(topic=topic, msg=msg)
+        print(f"{self.name} :: Published data")
+
+
+class DHTController:
+    def __init__(self, pin: str | int, name="DHT11"):
+        self.sensor = DHT11(machine.Pin(pin, machine.Pin.OUT))
+        self.name = name
+
+    def measure(self):
+        try:
+            self.sensor.measure()
+            return self
+        except Exception as e:
+            if isinstance(e, OSError) and e.args[0] == 110:
+                print(f"{self.name} :: Timed out")
+            else:
+                print(f"{self.name} :: Exception :: {str(e)}")
+            machine.reset()
+
+    def get_temperature(self):
+        return self.sensor.temperature()
+
+    def get_humidity(self):
+        return self.sensor.humidity()
+
+
+def main():
+    WLANController(ssid=SSID, ssid_secret=SSID_SECRET)
+    client = MQTTController(client_id=hexlify(machine.unique_id()),
+                            host=HOST,
+                            port=PORT,
+                            user=ADA_USER,
+                            secret=ADA_SECRET)
+    sensor = DHTController(pin=28)
+
+    while True:
+        msg = f"{sensor.measure().get_temperature()}".encode()
+        client.publish(topic=ADA_TOPIC.encode(), msg=msg)
+        utime.sleep(5)
+
+
+if __name__ == "__main__":
+    main()
