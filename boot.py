6361db9b9650d6ea533a8c3aab26a0caad286089
Samuel Svensson <samme.s90@gmail.com>
2024-05-08 11:26:36 +0200

    Add further error handling and prevent incorrect usage

--- boot.py
+++ boot.py
@@ -2,11 +2,35 @@ import machine
 import ujson
 import utime
 from dht import DHT11
-from network import STA_IF, WLAN
+from network import (STA_IF, STAT_CONNECT_FAIL, STAT_CONNECTING, STAT_GOT_IP,
+                     STAT_IDLE, STAT_NO_AP_FOUND, STAT_WRONG_PASSWORD, WLAN)
 from ubinascii import hexlify
 
 from config import ADA_SECRET, ADA_USER, HOST, PORT, SSID, SSID_SECRET
 from umqttsimple import MQTTClient
+
+
+# Workaround for MicroPython due to ABC not working
+# Should perhaps be placed in a seperate file named abc.py
+def abstractmethod(f):
+    return f
+
+
+class CallbackStrategy:
+    @abstractmethod
+    def execute(self, feed: bytes, msg: bytes):
+        pass
+
+
+class SwitchCallbackStrategy(CallbackStrategy):
+    def __init__(self, pin):
+        self.led = machine.Pin("WL_GPIO0", machine.Pin.OUT)
+
+    def execute(self, feed: bytes, msg: bytes):
+        if msg == b"ON":
+            self.led.on()
+        elif msg == b"OFF":
+            self.led.off()
 
 
 class Controller:
@@ -43,9 +67,16 @@ class WLANController(Controller):
         self.ssid = ssid
         self.ssid_secret = ssid_secret
         self.timeout = timeout
-        self.__connect()
+        self.stat_map = {
+            STAT_IDLE: "No connection and no activity",
+            STAT_CONNECTING: "Connecting in progress",
+            STAT_WRONG_PASSWORD: "Failed due to incorrect password",
+            STAT_NO_AP_FOUND: "Failed because no access point replied",
+            STAT_CONNECT_FAIL: "Failed due to other problems",
+            STAT_GOT_IP: "Connection successful"
+        }
 
-    def __connect(self):
+    def connect(self):
         try:
             if not self.sta_if.isconnected():
                 self.__print("Connecting")
@@ -56,7 +87,7 @@ class WLANController(Controller):
                 while not self.sta_if.isconnected():
                     utime.sleep(1)
                     if (elapsed := elapsed + 1) > self.timeout:
-                        raise Exception("Timed out")
+                        raise Exception(self.stat_map[self.sta_if.status()])
 
                 self.__print(f"Connected<{self.sta_if.ifconfig()}>")
         except Exception as e:
@@ -75,29 +106,42 @@ class MQTTController(Controller):
             secret,
             keepalive=60)
         self.info = f"{client_id.decode()}@{host}:{port}"
+        self.connected = False
 
-        self.__connect()
-
-    def __connect(self):
+    def connect(self):
         try:
             self.__print(f"Connecting<{self.info}>")
             self.client.connect()
+            self.connected = True
             self.__print("Connected")
         except Exception as e:
             self.__handle_exc(e)
 
     def update(self):
+        '''
+        Asyncronous method that checks the latest message.
+        '''
+        self.__check_connection()
+
         self.client.check_msg()
         self.__print("Updated")
 
-    def subscribe(self, feed: bytes, callback):
-        self.client.set_callback(f=callback)
+    def subscribe(self, feed: bytes, callback: CallbackStrategy):
+        self.__check_connection()
+
+        self.client.set_callback(f=callback.execute)
         self.client.subscribe(topic=feed)
         self.__print("Subscribed")
 
     def publish(self, feed: bytes, msg: bytes):
+        self.__check_connection()
+
         self.client.publish(topic=feed, msg=msg)
         self.__print("Published data")
+
+    def __check_connection(self):
+        if not self.connected:
+            self.__handle_exc(Exception("Not connected"))
 
 
 class DHTController(Controller):
@@ -120,22 +164,19 @@ class DHTController(Controller):
         return self.sensor.humidity()
 
 
-class App:
+class System:
     def __init__(self):
-        self.wlan = WLANController(SSID, SSID_SECRET, 5)
+        self.wlan = WLANController(SSID, SSID_SECRET, 1)
         self.mqtt = MQTTController(hexlify(machine.unique_id()), HOST, PORT, ADA_USER, ADA_SECRET)
         self.sensor = DHTController(28)
-        self.sensor_feed = f"{ADA_USER}/f/sensor".encode()
-        self.led = machine.Pin("WL_GPIO0", machine.Pin.OUT)
-        self.led_feed = f"{ADA_USER}/f/led".encode()
+        self.callback = SwitchCallbackStrategy("WL_GPIO0")
 
-        self.mqtt.subscribe(self.led_feed, self.sub_callback)
+        self.f_sensor = f"{ADA_USER}/f/sensor".encode()
+        self.f_led = f"{ADA_USER}/f/led".encode()
 
-    def sub_callback(self, feed: bytes, msg: bytes):
-        if msg == b"ON":
-            self.led.on()
-        elif msg == b"OFF":
-            self.led.off()
+        self.wlan.connect()
+        self.mqtt.connect()
+        self.mqtt.subscribe(self.f_led, self.callback)
 
     def run(self, interval=3):
         while True:
@@ -147,10 +188,10 @@ class App:
                 "humidity": self.sensor.get_humidity()
             })
             self.mqtt.publish(
-                feed=self.sensor_feed,
+                feed=self.f_sensor,
                 msg=data.encode())
 
             utime.sleep(interval)
 
 
-App().run()
+System().run()
